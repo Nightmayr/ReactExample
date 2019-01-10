5c47047085fe9b46e9857f0527244c26633eab82
Umayr <umayr23@gmail.com>
2019-01-10 15:04:33 +0000

    Used event handlers to update content on page

--- src/StateComponent.js
+++ src/StateComponent.js
@@ -3,27 +3,45 @@ import React, { Component } from 'react';
 class StateComponent extends Component{
     constructor(){
         super();
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.state = {
-            stateText: "Example text",
+            stateText: "Example text"
         }
         // this.update = () => this.setState({
         //     stateText: "New Text"
         // })
     }
 
-    update =  () => {
-        this.setState({
-            stateText: this.state.stateText = "New Text"
-        })
-    }
+    // update = () => {
+    //     this.setState({
+    //         stateText: this.state.stateText = "New Text"
+    //     })
+    // }
+
+    handleChange = (event) => {
+        this.setState({stateText: document.getElementById("text").value});
+      }
+
+      handleSubmit(event) {
+        console.log('A name was submitted: ' + this.state.stateText);
+        event.preventDefault();
+      }
+
+    //   handleSubmit = (event) => {
+    //     console.log('A name was submitted: ' + this.state.stateText);
+    //     event.preventDefault();
+    //   }
 
     render(){
         return(
             <div>
                 <h1>{this.state.stateText}</h1>
                 <h3>
-                    <input type="text" ref="updateText/"></input>
-                    <button onClick={this.update}>Update Text</button>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>Text:</label>
+                        <input id="text" type="text"></input>
+                        <input type="submit" value="Submit" onClick={this.handleChange}></input>
+                    </form>
                 </h3>
             </div>
         );
